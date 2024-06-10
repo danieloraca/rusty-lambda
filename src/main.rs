3b00acd3656051d1155e64f386b0da1805b87972
Dan <daniel.oraca@gmail.com>
2024-06-10 20:10:02 +0100

    rusty lambda tweak monday

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod util;
+
 use aws_config::{self, BehaviorVersion};
 use aws_sdk_s3::primitives::ByteStream;
 use aws_sdk_s3::Client;
@@ -5,7 +7,6 @@ use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 use reqwest::get;
 use serde::Serialize;
 use serde_json::Value;
-use url::Url;
 
 #[derive(Serialize)]
 struct LambdaResponse {
@@ -15,55 +16,17 @@ struct LambdaResponse {
 
 async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let payload: Value = event.payload;
-    tracing::info!("Payload: {}", payload);
 
-    let body_json: Value = serde_json::from_str(
-        payload["body"]
-            .as_str()
-            .ok_or_else(|| anyhow::anyhow!("Invalid body"))?,
-    )
-    .map_err(|e| -> Error { anyhow::anyhow!("Error parsing JSON: {}", e).into() })?;
-    tracing::info!("Body JSON: {}", body_json);
-
-    let image_url: &str = body_json["image_url"]
-        .as_str()
-        .ok_or_else(|| anyhow::anyhow!("No image url found in payload"))?;
-    tracing::info!("Image URL: {}", image_url);
-
-    let image_new_size: &str = body_json["image_new_size"]
-        .as_str()
-        .ok_or_else(|| anyhow::anyhow!("No image new size found in payload"))?;
-    tracing::info!("Image new size: {}", image_new_size);
-
-    let bucket_name: String =
-        std::env::var("THE_BUCKET_NAME").expect("THE_BUCKET_NAME must be set");
-    tracing::info!("Bucket name: {}", bucket_name);
-
-    let image_name: String = match Url::parse(image_url) {
-        Ok(parsed_url) => {
-            if let Some(name) = parsed_url
-                .path_segments()
-                .and_then(|segments| segments.last())
-            {
-                name.to_string()
-            } else {
-                return Err("No image name found in the URL.".into());
-            }
-        }
-        Err(e) => {
-            return Err(format!("Failed to parse URL: {}", e).into());
-        }
-    };
+    let (image_url, _image_new_size) = util::original_image_info(&payload)?;
+    let bucket_name: String = util::get_bucket_name();
+    let image_name: String = util::get_image_name(&image_url)?;
 
     let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let client = Client::new(&config);
     tracing::info!("Client created");
 
-    let resp = get(image_url).await?;
+    let resp = get(&image_url).await?;
     let image_bytes = resp.bytes().await?;
-
-    // Upload the image to S3
-    // let mut img_name = "uploaded_image.jpg";
 
     client
         .put_object()
