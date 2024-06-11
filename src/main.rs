34a09988850b8e2b4b07b054b76dcee3d27eb8f4
Dan <daniel.oraca@gmail.com>
2024-06-11 20:41:08 +0100

    rusty lambda tweak

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// mod util;
-
 use aws_config::{self, BehaviorVersion};
 use aws_sdk_s3::primitives::ByteStream;
 use aws_sdk_s3::Client;
@@ -10,14 +8,13 @@ use serde_json::Value;
 use url::Url;
 
 #[derive(Serialize)]
-struct Response {
+struct LambdaResponse {
     req_id: String,
     msg: String,
 }
 
 async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let payload: Value = event.payload;
-
     tracing::info!("Payload: {}", payload);
 
     let body_json: Value = serde_json::from_str(
@@ -28,30 +25,21 @@ async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     .map_err(|e| -> Error { anyhow::anyhow!("Error parsing JSON: {}", e).into() })?;
     tracing::info!("Body JSON: {}", body_json);
 
-    let image_url = body_json["image_url"]
+    let image_url: &str = body_json["image_url"]
         .as_str()
         .ok_or_else(|| anyhow::anyhow!("No image url found in payload"))?;
     tracing::info!("Image URL: {}", image_url);
 
-    let image_new_size = body_json["image_new_size"]
+    let image_new_size: &str = body_json["image_new_size"]
         .as_str()
         .ok_or_else(|| anyhow::anyhow!("No image new size found in payload"))?;
     tracing::info!("Image new size: {}", image_new_size);
 
-    let bucket_name = std::env::var("THE_BUCKET_NAME").expect("THE_BUCKET_NAME must be set");
+    let bucket_name: String =
+        std::env::var("THE_BUCKET_NAME").expect("THE_BUCKET_NAME must be set");
     tracing::info!("Bucket name: {}", bucket_name);
 
-    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
-    let client = Client::new(&config);
-    tracing::info!("Client created");
-
-    let resp = get(image_url).await?;
-    let image_bytes = resp.bytes().await?;
-
-    // Upload the image to S3
-    // let mut img_name = "uploaded_image.jpg";
-
-    let image_name = match Url::parse(image_url) {
+    let image_name: String = match Url::parse(image_url) {
         Ok(parsed_url) => {
             if let Some(name) = parsed_url
                 .path_segments()
@@ -67,6 +55,16 @@ async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
         }
     };
 
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let client = Client::new(&config);
+    tracing::info!("Client created");
+
+    let resp = get(image_url).await?;
+    let image_bytes = resp.bytes().await?;
+
+    // Upload the image to S3
+    // let mut img_name = "uploaded_image.jpg";
+
     client
         .put_object()
         .bucket(&bucket_name)
@@ -74,13 +72,17 @@ async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
         .body(ByteStream::from(image_bytes.to_vec()))
         .send()
         .await?;
+
     // Prepare the response
-    let resp = Response {
+    let resp = LambdaResponse {
         req_id: event.context.request_id,
         msg: format!("Uploaded image {}", image_url),
     };
+
     let resp_json = serde_json::to_string(&resp)?;
+
     tracing::info!("Response in CloudWatch: {}", resp_json);
+
     Ok(serde_json::json!({
         "statusCode": 200,
         "body": resp_json,
