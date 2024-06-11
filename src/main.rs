51ccc47afea32ce725110e09f0b56203f1f0c560
Dan <daniel.oraca@gmail.com>
2024-06-11 20:26:10 +0100

    rusty lambda tweak tuesday

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod util;
+// mod util;
 
 use aws_config::{self, BehaviorVersion};
 use aws_sdk_s3::primitives::ByteStream;
@@ -7,6 +7,7 @@ use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 use reqwest::get;
 use serde::Serialize;
 use serde_json::Value;
+use url::Url;
 
 #[derive(Serialize)]
 struct LambdaResponse {
@@ -17,9 +18,45 @@ struct LambdaResponse {
 async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let payload: Value = event.payload;
 
-    let (image_url, _image_new_size) = util::original_image_info(&payload)?;
-    let bucket_name: String = util::get_bucket_name();
-    let image_name: String = util::get_image_name(&image_url)?;
+    tracing::info!("Payload: {}", payload);
+
+    let body_json: Value = serde_json::from_str(
+        payload["body"]
+            .as_str()
+            .ok_or_else(|| anyhow::anyhow!("Invalid body"))?,
+    )
+    .map_err(|e| -> Error { anyhow::anyhow!("Error parsing JSON: {}", e).into() })?;
+    tracing::info!("Body JSON: {}", body_json);
+
+    let image_url: &str = body_json["image_url"]
+        .as_str()
+        .ok_or_else(|| anyhow::anyhow!("No image url found in payload"))?;
+    tracing::info!("Image URL: {}", image_url);
+
+    let image_new_size: &str = body_json["image_new_size"]
+        .as_str()
+        .ok_or_else(|| anyhow::anyhow!("No image new size found in payload"))?;
+    tracing::info!("Image new size: {}", image_new_size);
+
+    let bucket_name: String =
+        std::env::var("THE_BUCKET_NAME").expect("THE_BUCKET_NAME must be set");
+    tracing::info!("Bucket name: {}", bucket_name);
+
+    let image_name: String = match Url::parse(image_url) {
+        Ok(parsed_url) => {
+            if let Some(name) = parsed_url
+                .path_segments()
+                .and_then(|segments| segments.last())
+            {
+                name.to_string()
+            } else {
+                return Err("No image name found in the URL.".into());
+            }
+        }
+        Err(e) => {
+            return Err(format!("Failed to parse URL: {}", e).into());
+        }
+    };
 
     let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let client = Client::new(&config);
@@ -28,6 +65,9 @@ async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let resp = get(image_url).await?;
     let image_bytes = resp.bytes().await?;
 
+    // Upload the image to S3
+    // let mut img_name = "uploaded_image.jpg";
+
     client
         .put_object()
         .bucket(&bucket_name)
@@ -35,18 +75,13 @@ async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
         .body(ByteStream::from(image_bytes.to_vec()))
         .send()
         .await?;
-
     // Prepare the response
     let resp = LambdaResponse {
         req_id: event.context.request_id,
-        // msg: format!("Uploaded image {}", image_url),
-        msg: format!("Uploaded image ..."),
+        msg: format!("Uploaded image {}", image_url),
     };
-
     let resp_json = serde_json::to_string(&resp)?;
-
     tracing::info!("Response in CloudWatch: {}", resp_json);
-
     Ok(serde_json::json!({
         "statusCode": 200,
         "body": resp_json,
